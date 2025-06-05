deee5cb038974d674fbd6bf0a921f4e7dabacf57
Isabelly Esmeralda <isa.esmeralda27@gmail.com>
2025-06-05 17:48:20 -0300

    2

--- app/home/page.js
+++ app/home/page.js
@@ -1,27 +1,20 @@
-import Section1 from "../ui/home/Section1"
-<<<<<<< HEAD
-import Section4 from "../ui/home/Section4"
-=======
 import Header from "../ui/home/Header"
+import Section1 from "../ui/home/Section1"
+import Section4 from "../ui/home/Section4"
 import Section2 from "../ui/home/Section2"
 import Section3 from "../ui/home/Section3"
 import Section5 from "../ui/home/Section5"
 
->>>>>>> fb30509f383ec288bb2eddf25e457f05764b92b3
 
 export default function Home(){
     return(
       <>
         <Header/>
         <Section1/>
-<<<<<<< HEAD
-        <Section4/>
-
-=======
         <Section2/>
         <Section3/>
+        <Section4/>
         <Section5/>
->>>>>>> fb30509f383ec288bb2eddf25e457f05764b92b3
       </>
     )
   }
