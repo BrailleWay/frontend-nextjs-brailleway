f31df59612dd67fcb35b995a17d2fc6e02d27ed0
RenanSantos0 <renansantosreis0@gmail.com>
2025-06-20 17:08:32 -0300

    Refactor app structure and remove unused files
    Renamed layout.jsx to layout.js and replaced page.jsx with a simplified page.js. Removed lib/actions.js and its related server-side logic, indicating a cleanup or refactor of authentication and database actions.

--- app/page.jsx
+++
@@ -1,8 +0,0 @@
-export default function Home(){
-  return(
-    <>
-      <h1 className="text-3xl bg-black text-teal-300 ">mdamdma</h1>
-      <div className="size-30 bg-yellow-500"></div>
-    </>
-  )
-}
