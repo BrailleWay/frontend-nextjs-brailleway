4cd5da55f631e8c962c27add2234cd972a695e89
Marcio Eduardo Ordalio Costa <balomateed384@gmail.com>
2025-06-29 22:36:00 -0300

    Correção1

--- app/layout.js
+++ app/layout.js
@@ -1,26 +1,28 @@
 // app/layout.js
 import "./globals.css";
-import Header from "./ui/home/Header.js";
+import Header from "./ui/home/Header";
 import Footer from "./ui/home/Footer";
 import { Urbanist, Inter, Poppins } from "next/font/google";
 
+// Fonte principal/padrão do site
 const inter = Inter({
   subsets: ["latin"],
-  display: "swap",
-  variable: "--font-inter",
+  display: "swap", // Melhora o carregamento
+  variable: "--font-inter", // Opcional, mas boa prática
 });
 
+// Fontes secundárias como variáveis
 const urbanist = Urbanist({
   subsets: ["latin"],
   display: "swap",
-  variable: "--font-urbanist",
+  variable: "--font-urbanist", // Cria a variável CSS
 });
 
 const poppins = Poppins({
   subsets: ["latin"],
   weight: ["400", "500", "600", "700"],
   display: "swap",
-  variable: "--font-poppins",
+  variable: "--font-poppins", // Cria a variável CSS
 });
 
 export const metadata = {
